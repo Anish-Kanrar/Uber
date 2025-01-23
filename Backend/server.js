31cc98d58fe252b9cfc46da00efb7d5ab52ad988
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2025-01-23 23:27:46 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const https = require('http');
+const app = require('./app');
+// const { log } = require('console');
+const port = process.env.PORT || 3000;
+
+const server = https.createServer(app);
+
+server.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
